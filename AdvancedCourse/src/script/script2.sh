250cceba7ad3e04b30309a44a7096a68da18de28
AhnCoder <88716899+AhnCoder@users.noreply.github.com>
2024-02-21 19:40:12 +0900

    Update script2.sh

--- AdvancedCourse/src/script/script2.sh
+++ AdvancedCourse/src/script/script2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#section_1
+echo "kakaocloud: section_1"
 
 echo "kakaocloud: Preparing directories and files"
 
@@ -10,7 +10,7 @@ echo "kakaocloud: Directories prepared"
 
 
 
-#section_2
+echo "kakaocloud: section_2"
 
 echo "kakaocloud: Downloading YAML files"
 
@@ -26,7 +26,7 @@ echo "kakaocloud: All YAML files downloaded"
 
 
 
-# section_3
+echo "kakaocloud: section_3"
 
 echo "kakaocloud: Installing kubectl"
 
@@ -38,7 +38,7 @@ echo "kakaocloud: kubectl installed"
 
 
 
-#section_4
+echo "kakaocloud: section_4"
 
 echo "kakaocloud: Setting up .kube directory and configuration file"
 
@@ -50,7 +50,7 @@ echo "kakaocloud: .kube setup completed"
 
 
 
-#section_5
+echo "kakaocloud: section_5"
 
 echo "kakaocloud: Installing kic-iam-auth"
 
@@ -62,7 +62,7 @@ sudo mv kic-iam-auth /usr/local/bin/ || { echo "Failed to move kic-iam-auth to /
 
 echo "kakaocloud: kic-iam-auth installation completed"
 
-#section_6
+echo "kakaocloud: section_6"
 echo "kakaocloud: Installing Docker and setting it up"
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo "Failed to download Docker GPG key"; exit 1; }
 sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || { echo "Failed to add Docker repository to sources.list"; exit 1; }
@@ -71,19 +71,19 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io || { echo "Failed 
 sudo chmod 666 /var/run/docker.sock || { echo "Failed to change permission of docker.sock"; exit 1; }
 echo "kakaocloud: Docker installation and setup completed"
 
-#section_7
+echo "kakaocloud: section_7"
 echo "kakaocloud: Installing additional software"
 sudo apt install unzip || { echo "Failed to install unzip"; exit 1; }
 sudo apt-get install -y openjdk-17-jdk maven || { echo "Failed to install openjdk-17-jdk and maven"; exit 1; }
 echo "kakaocloud: Additional software installation completed"
 
-#section_8
+echo "kakaocloud: section_8"
 echo "kakaocloud: Setting permissions for .kube/config"
 chmod 600 /home/ubuntu/.kube/config || { echo "Failed to change permission of /home/ubuntu/.kube/config"; exit 1; }
 chown ubuntu:ubuntu /home/ubuntu/.kube/config || { echo "Failed to change ownership of /home/ubuntu/.kube/config"; exit 1; }
 echo "kakaocloud: Permissions set for .kube/config"
 
-#section_9
+echo "kakaocloud: section_9"
 echo "kakaocloud: Downloading helm-values.yaml and applying environment substitutions"
 sudo curl -o /home/ubuntu/values.yaml https://raw.githubusercontent.com/kakaocloud-edu/tutorial/main/AdvancedCourse/src/manifests/helm-values.yaml || { echo "Failed to download helm-values.yaml"; exit 1; }
 
