211ed0257a9c27a3a540ce891fa44321c7c8ab31
AhnCoder <88716899+AhnCoder@users.noreply.github.com>
2024-02-21 20:27:26 +0900

    Create script3.sh

---
+++ AdvancedCourse/src/script/script3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-1.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-2.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-3.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-4.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-5.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-6.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-7.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-8.sh \
+     https://github.com/kakaocloud-edu/tutorial/raw/main/AdvancedCourse/src/script/script3-9.sh && \
+sudo chmod +x script3-*.sh
+
+
+sudo source ./script3-1.sh
+sudo source ./script3-2.sh
+sudo source ./script3-3.sh
+sudo source ./script3-4.sh
+sudo source ./script3-5.sh
+sudo source ./script3-6.sh
+sudo source ./script3-7.sh
+sudo source ./script3-8.sh
+sudo source ./script3-9.sh
