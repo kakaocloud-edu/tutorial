b2eeed32b69e7d510b01da15cb7fd0a5cdaffd13
AhnCoder <88716899+AhnCoder@users.noreply.github.com>
2024-02-22 18:33:47 +0900

    Delete AdvancedCourse/src/script/script3-8.sh

--- AdvancedCourse/src/script/script3-8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "kakaocloud: section_8"
-echo "kakaocloud: Setting permissions for .kube/config"
-sudo chmod 600 /home/ubuntu/.kube/config || { echo "Failed to change permission of /home/ubuntu/.kube/config"; exit 1; }
-sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config || { echo "Failed to change ownership of /home/ubuntu/.kube/config"; exit 1; }
-echo "kakaocloud: Permissions set for .kube/config"
