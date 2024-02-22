957ac1fb5bc8fb5c52f140a414b3f16495562077
AhnCoder <88716899+AhnCoder@users.noreply.github.com>
2024-02-22 18:33:54 +0900

    Delete AdvancedCourse/src/script/script3-7.sh

--- AdvancedCourse/src/script/script3-7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "kakaocloud: section_7"
-echo "kakaocloud: Installing additional software"
-sudo apt install unzip || { echo "Failed to install unzip"; exit 1; }
-sudo apt-get install -y openjdk-17-jdk maven || { echo "Failed to install openjdk-17-jdk and maven"; exit 1; }
-echo "kakaocloud: Additional software installation completed"
