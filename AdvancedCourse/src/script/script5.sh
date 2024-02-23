03a80dbf3b5196f9a1bfdfcae804544bb30a2450
JOOONU <127095828+NOOJU@users.noreply.github.com>
2024-02-23 23:56:33 +0900

    Update script5.sh

--- AdvancedCourse/src/script/script5.sh
+++ AdvancedCourse/src/script/script5.sh
@@ -76,7 +76,6 @@ echo "kakaocloud: Permissions set for .kube/config"
 echo "kakaocloud: 13.Downloading helm-values.yaml and applying environment substitutions"
 sudo cp /home/ubuntu/tutorial/AdvancedCourse/src/manifests/helm-values.yaml /home/ubuntu/values.yaml || { echo "Failed to download helm-values.yaml"; exit 1; }
 envsubst < /home/ubuntu/.kube/config > /home/ubuntu/.kube/config.tmp && mv /home/ubuntu/.kube/config.tmp /home/ubuntu/.kube/config || { echo "Failed to modify /home/ubuntu/.kube/config"; exit 1; }
-envsubst < /home/ubuntu/yaml/lab6-Deployment.yaml > /home/ubuntu/yaml/deploy.tmp && mv /home/ubuntu/yaml/deploy.tmp /home/ubuntu/yaml/lab6-Deployment.yaml || { echo "Failed to modify /home/ubuntu/yaml/lab6-Deployment.yaml"; exit 1; }
-envsubst < /home/ubuntu/yaml/lab6-Secret.yaml > /home/ubuntu/yaml/secret.tmp && mv /home/ubuntu/yaml/secret.tmp /home/ubuntu/yaml/lab6-Secret.yaml || { echo "Failed to modify /home/ubuntu/yaml/lab6-Secret.yaml"; exit 1; }
+envsubst < /home/ubuntu/yaml/lab6-manifest.yaml > /home/ubuntu/yaml/manifest.tmp && mv /home/ubuntu/yaml/manifest.tmp /home/ubuntu/yaml/lab6-manifest.yaml || { echo "Failed to modify /home/ubuntu/yaml/lab6-manifests.yaml"; exit 1; }
 envsubst < /home/ubuntu/values.yaml > /home/ubuntu/values.tmp && mv /home/ubuntu/values.tmp /home/ubuntu/values.yaml || { echo "Failed to modify /home/ubuntu/values.yaml"; exit 1; }
 echo "kakaocloud: Environment substitutions applied and setup completed"
