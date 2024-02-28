620b358ab65941ed39e2e2a2fc81ef454c03ed61
JOOONU <127095828+NOOJU@users.noreply.github.com>
2024-02-28 23:17:32 +0900

    Update script5.sh

--- AdvancedCourse/src/script/script5.sh
+++ AdvancedCourse/src/script/script5.sh
@@ -69,7 +69,7 @@ echo "kakaocloud: Setting .kube/config completed"
 echo "kakaocloud: 12.Downloading helm-values.yaml and applying environment substitutions"
 sudo cp /home/ubuntu/tutorial/AdvancedCourse/src/manifests/helm-values.yaml /home/ubuntu/values.yaml || { echo "kakaocloud: Failed to download helm-values.yaml"; exit 1; }
 envsubst < /home/ubuntu/.kube/config > /home/ubuntu/.kube/config.tmp && mv /home/ubuntu/.kube/config.tmp /home/ubuntu/.kube/config || { echo "kakaocloud: Failed to modify /home/ubuntu/.kube/config"; exit 1; }
-envsubst < /home/ubuntu/yaml/lab6-manifest.yaml > /home/ubuntu/yaml/manifest.tmp && mv /home/ubuntu/yaml/manifest.tmp /home/ubuntu/yaml/lab6-manifest.yaml || { echo "kakaocloud: Failed to modify /home/ubuntu/yaml/lab6-manifests.yaml"; exit 1; }
+envsubst < /home/ubuntu/yaml/lab6-manifests.yaml > /home/ubuntu/yaml/manifests.tmp && mv /home/ubuntu/yaml/manifests.tmp /home/ubuntu/yaml/lab6-manifests.yaml || { echo "kakaocloud: Failed to modify /home/ubuntu/yaml/lab6-manifests.yaml"; exit 1; }
 envsubst < /home/ubuntu/values.yaml > /home/ubuntu/values.tmp && mv /home/ubuntu/values.tmp /home/ubuntu/values.yaml || { echo "kakaocloud: Failed to modify /home/ubuntu/values.yaml"; exit 1; }
 echo "kakaocloud: Environment substitutions applied and setup completed"
 
