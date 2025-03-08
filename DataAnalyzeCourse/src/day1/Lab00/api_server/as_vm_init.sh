e4e94bdc51936459b45cd72cfc249023c481bd03
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-03-08 22:46:37 +0900

    Update as_vm_init.sh

--- DataAnalyzeCourse/src/day1/Lab00/api_server/as_vm_init.sh
+++ DataAnalyzeCourse/src/day1/Lab00/api_server/as_vm_init.sh
@@ -6,9 +6,11 @@ PROJECT_ID="{프로젝트 ID}"
 CREDENTIAL_ID="{액세스 키 ID}"
 CREDENTIAL_SECRET="{보안 액세스 키}"
 LOGSTASH_KAFKA_ENDPOINT="{Kafka 클러스터 부트스트랩 서버}"
+
 PUBSUB_TOPIC_NAME="log-topic"
 KAFKA_TOPIC_NAME="nginx-topic"
 LOGSTASH_ENV_FILE="/etc/default/logstash"
+ENV_SETUP_SCRIPT_URL="https://github.com/kakaocloud-edu/tutorial/raw/refs/heads/main/DataAnalyzeCourse/src/day1/Lab00/api_server/as_env_setup.sh"
 
 echo "kakaocloud: 1. ~/.bashrc에 환경 변수를 설정합니다."
 
@@ -34,16 +36,15 @@ source /home/ubuntu/.bashrc
 echo "kakaocloud: ~/.bashrc에 환경 변수를 추가 완료."
 
 echo "kakaocloud: 2. as_env_setup.sh 스크립트를 다운로드합니다."
-SECOND_SCRIPT_URL="https://github.com/lys0611/DA-api-svr-script/raw/refs/heads/main/as_env_setup.sh"
 
 # 유효성 체크
-curl --output /dev/null --silent --head --fail "$SECOND_SCRIPT_URL" || {
+curl --output /dev/null --silent --head --fail "$ENV_SETUP_SCRIPT_URL" || {
   echo "kakaocloud: as_env_setup.sh 다운로드 링크가 유효하지 않습니다."
   exit 1
 }
 
 echo "kakaocloud: 스크립트 다운로드 링크가 유효합니다. 스크립트를 다운로드합니다."
-wget -O as_env_setup.sh "$SECOND_SCRIPT_URL"
+wget -O as_env_setup.sh "$ENV_SETUP_SCRIPT_URL"
 chmod +x as_env_setup.sh
 
 echo "kakaocloud: as_env_setup.sh 실행을 시작합니다."
