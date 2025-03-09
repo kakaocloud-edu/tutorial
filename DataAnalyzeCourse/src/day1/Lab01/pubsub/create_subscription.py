d02944420a762f6b07e1037e5f0ff4c04c40cd89
cjm0423 <87014584+cjm0423@users.noreply.github.com>
2025-03-09 22:07:58 +0900

    Update create_subscription.py

--- DataAnalyzeCourse/src/day1/Lab01/pubsub/create_subscription.py
+++ DataAnalyzeCourse/src/day1/Lab01/pubsub/create_subscription.py
@@ -14,11 +14,12 @@ import os
 # 현재 스크립트의 디렉토리 경로 가져오기
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# 상위 디렉토리 경로 가져오기 (REST API의 상위 디렉토리는 TrafficGenerator)
-parent_dir = os.path.dirname(current_dir)
+# config.py가 위치한 경로:
+# 현재 디렉토리에서 두 단계 위로 이동한 후 Lab00/traffic_generator 디렉토리로 이동
+config_path = os.path.abspath(os.path.join(current_dir, "..", "..", "Lab00", "traffic_generator"))
 
-# 상위 디렉토리를 Python 경로에 추가하여 config.py를 임포트 가능하게 함
-sys.path.append(parent_dir)
+#절대경로 지정
+sys.path.append(config_path)
 
 import config
 
