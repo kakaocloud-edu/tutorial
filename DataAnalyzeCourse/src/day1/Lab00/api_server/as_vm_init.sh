972d14796392fd3e19f16404fe3d9dd2f98cf866
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-03-13 21:41:22 +0900

    Update as_vm_init.sh

--- DataAnalyzeCourse/src/day1/Lab00/api_server/as_vm_init.sh
+++ DataAnalyzeCourse/src/day1/Lab00/api_server/as_vm_init.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export MYSQL_HOST="{MySQL 엔드포인트}"
+# 프로젝트 및 인증 정보
 export DOMAIN_ID="{조직 ID}"
 export PROJECT_ID="{프로젝트 ID}"
 export CREDENTIAL_ID="{액세스 키 ID}"
 export CREDENTIAL_SECRET="{보안 액세스 키}"
-export LOGSTASH_KAFKA_ENDPOINT="temp-kafka-bootstrap-server"
-
+# 데이터베이스 설정
+export MYSQL_HOST="{MySQL 엔드포인트}"
+# Pub/Sub 및 Kafka 설정
 export PUBSUB_TOPIC_NAME="log-topic"
 export KAFKA_TOPIC_NAME="nginx-topic"
+export LOGSTASH_KAFKA_ENDPOINT="temp-kafka-bootstrap-server"
+# 로그 및 환경 설정
 export LOGSTASH_ENV_FILE="/etc/default/logstash"
 export ENV_SETUP_SCRIPT_URL="https://github.com/kakaocloud-edu/tutorial/raw/refs/heads/main/DataAnalyzeCourse/src/day1/Lab00/api_server/as_env_setup.sh"
 
