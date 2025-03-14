9fe54fa3aa0b991724f6bd14dec9ab3b7534a2f5
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-03-14 21:25:02 +0900

    Update data_catalog_subscribe.py

--- DataAnalyzeCourse/src/day1/Lab03/data_catalog_subscribe.py
+++ DataAnalyzeCourse/src/day1/Lab03/data_catalog_subscribe.py
@@ -17,13 +17,14 @@ import os
 # 현재 스크립트의 디렉토리 경로 가져오기
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# config.py가 위치한 경로:
-# 현재 디렉토리에서 두 단계 위로 이동한 후 Lab00/traffic_generator 디렉토리로 이동
-config_path = os.path.abspath(os.path.join(current_dir, "..", "..", "Lab00", "traffic_generator"))
+# config.py가 있는 Lab00/traffic_generator 경로를 sys.path에 추가
+config_path = os.path.abspath(os.path.join(current_dir, "..", "Lab00", "traffic_generator"))
+sys.path.append(config_path)
 
 #절대경로 지정
 sys.path.append(config_path)
 
+# config.py 가져오기
 from config import *
 
 #################################
