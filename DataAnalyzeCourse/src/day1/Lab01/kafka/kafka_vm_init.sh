532549fd24b37a9495f93f6b0f5518d5a7162354
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-04-27 19:38:48 +0900

    Update kafka_vm_init.sh

--- DataAnalyzeCourse/src/day1/Lab01/kafka/kafka_vm_init.sh
+++ DataAnalyzeCourse/src/day1/Lab01/kafka/kafka_vm_init.sh
@@ -24,7 +24,7 @@ source /tmp/env_vars.sh
 echo "source /tmp/env_vars.sh" >> /home/ubuntu/.bashrc
 
 echo "kakaocloud: 2.스크립트 다운로드 사이트 유효성 검사 시작"
-SCRIPT_URL="https://raw.githubusercontent.com/kakaocloud-edu/tutorial/refs/heads/main/DataAnalyzeCourse/src/day1/Lab00/kafka/kafka_full_setup.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/kakaocloud-edu/tutorial/refs/heads/main/DataAnalyzeCourse/src/day1/Lab01/kafka/kafka_full_setup.sh"
 
 curl -L --output /dev/null --silent --head --fail "$SCRIPT_URL" || { echo "kakaocloud: Script download site is not valid"; exit 1; }
 wget -q "$SCRIPT_URL"
