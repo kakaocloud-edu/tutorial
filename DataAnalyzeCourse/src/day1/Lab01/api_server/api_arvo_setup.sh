ed232fb0828fa377e4286d770f1f2a97fc710ac0
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-05-07 00:37:45 +0900

    Update api_arvo_setup.sh

--- DataAnalyzeCourse/src/day1/Lab01/api_server/api_arvo_setup.sh
+++ DataAnalyzeCourse/src/day1/Lab01/api_server/api_arvo_setup.sh
@@ -11,7 +11,7 @@ LOGSTASH_CONF="/etc/logstash/conf.d/logs-to-kafka.conf"
 AVSC_FILE="${LOGSTASH_SCHEMA_DIR}/nginx_log.avsc"
 
 # 1. Java & Confluent 설치
-echo "kakaocloud: apt 업데이트 및 Java 설치"
+echo "apt 업데이트 및 Java 설치"
 sudo apt-get update -y || {
     echo "kakaocloud: apt 업데이트 실패"; exit 1;
 }
@@ -19,7 +19,7 @@ sudo apt-get install -y openjdk-11-jdk wget || {
     echo "kakaocloud: Java 설치 실패"; exit 1;
 }
 
-echo "kakaocloud: Confluent 패키지 다운로드 및 설치"
+echo "Confluent 패키지 다운로드 및 설치"
 sudo wget https://packages.confluent.io/archive/7.5/confluent-community-7.5.3.tar.gz || {
     echo "kakaocloud: Confluent 패키지 다운로드 실패"; exit 1;
 }
@@ -30,7 +30,7 @@ sudo mv confluent-7.5.3 /opt/confluent || {
     echo "kakaocloud: Confluent 패키지 이동 실패"; exit 1;
 }
 
-echo "kakaocloud: 환경변수 설정"
+echo "환경변수 설정"
 grep -qxF "export CONFLUENT_HOME=${CONFLUENT_HOME}" ~/.bashrc || \
   echo 'export CONFLUENT_HOME=/opt/confluent' >> ~/.bashrc
 grep -qxF 'export PATH=$PATH:$CONFLUENT_HOME/bin' ~/.bashrc || \
@@ -40,12 +40,12 @@ export CONFLUENT_HOME="${CONFLUENT_HOME}"
 export PATH="$PATH:${CONFLUENT_HOME}/bin"
 
 # 2. Schema Registry 설정 & 서비스 등록
-echo "kakaocloud: schema-registry.properties 내 Kafka broker 주소 변경"
+echo "schema-registry.properties 내 Kafka broker 주소 변경"
 sudo sed -i 's|PLAINTEXT://localhost:9092|10.0.3.189:9092,10.0.2.254:9092|g' /opt/confluent/etc/schema-registry/schema-registry.properties || {
     echo "kakaocloud: Kafka broker 주소 변경 실패"; exit 1;
 }
 
-echo "kakaocloud: systemd 서비스 유닛 생성"
+echo "systemd 서비스 유닛 생성"
 sudo cp /home/ubuntu/tutorial/DataAnalyzeCourse/src/day1/Lab01/api_server/schema-registry.service /etc/logstash/conf.d/schema-registry.service || {
     echo "kakaocloud: schema-registry.service 복사 실패"; exit 1;
 }
@@ -53,7 +53,7 @@ sudo cp /home/ubuntu/tutorial/DataAnalyzeCourse/src/day1/Lab01/api_server/schema
     echo "kakaocloud: schema-registry.service 복사 실패"; exit 1;
 }
 
-echo "kakaocloud: systemd 데몬 리로드 및 schema-registry 서비스 활성화/시작"
+echo "systemd 데몬 리로드 및 schema-registry 서비스 활성화/시작"
 sudo systemctl daemon-reload || {
     echo "kakaocloud: systemd 데몬 리로드 실패"; exit 1;
 }
@@ -65,12 +65,12 @@ sudo systemctl start  schema-registry.service || {
 }
 
 # 3. Logstash Avro 플러그인 & 스키마
-echo "kakaocloud: Logstash Avro codec 플러그인 설치"
+echo "Logstash Avro codec 플러그인 설치"
 sudo /usr/share/logstash/bin/logstash-plugin install logstash-codec-avro_schema_registry || {
     echo "kakaocloud: Logstash Avro codec 플러그인 설치 실패"; exit 1;
 }
 
-echo "kakaocloud: Avro 스키마 및 Logstash 설정 파일 생성"
+echo "Avro 스키마 및 Logstash 설정 파일 생성"
 sudo mkdir -p /etc/logstash/schema || {
     echo "kakaocloud: Avro 스키마 및 Logstash 설정 파일 생성 실패"; exit 1;
 }
