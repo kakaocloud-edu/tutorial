988332281e182e5088c0a045e2074464aca971c5
KOlizer <127844467+KOlizer@users.noreply.github.com>
2025-07-08 13:59:10 +0900

    Update tg_vm_init.sh

--- DataAnalyzeCourse/src/day1/Lab01/traffic_generator/tg_vm_init.sh
+++ DataAnalyzeCourse/src/day1/Lab01/traffic_generator/tg_vm_init.sh
@@ -13,6 +13,7 @@ export CREDENTIAL_SECRET="{보안 액세스 키}"
 
 # 콘솔에서 생성한 리소스 정보
 export API_BASE_URL="{ALB의 Public IP}"
+export KAFKA_BOOTSTRAP_SERVERS="{Kafka 부트스트랩 서버}"
 export TOPIC_NAME="test-topic"
 export SUB_NAME="test-pull-sub"
 
