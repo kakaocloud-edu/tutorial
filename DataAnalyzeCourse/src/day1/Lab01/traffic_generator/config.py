2b9a8e3be1b85f5a6723e70ff20193ce9385c3e0
KOlizer <127844467+KOlizer@users.noreply.github.com>
2025-07-11 21:43:00 +0900

    Update config.py

--- DataAnalyzeCourse/src/day1/Lab01/traffic_generator/config.py
+++ DataAnalyzeCourse/src/day1/Lab01/traffic_generator/config.py
@@ -7,11 +7,10 @@ config.py
 
 import os
 import yaml
+import json
 
 # 현재 스크립트의 디렉토리 경로 가져오기
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# 상위 디렉토리 경로 가져오기 (REST API의 상위 디렉토리는 TrafficGenerator)
-parent_dir = os.path.dirname(current_dir)
 # config.yml 파일의 전체 경로 설정
 config_path = os.path.join(current_dir, "config.yml")
 
@@ -31,7 +30,6 @@ SUB_NAME = config['pubsub']['sub_name']
 CREDENTIAL_ID = config['pubsub']['credential_id']
 CREDENTIAL_SECRET = config['pubsub']['credential_secret']
 
-
 # Object Storage 서브스크립션 설정
 OBJECT_STORAGE_SUBSCRIPTION_NAME_MK = config['object_storage_subscription']['name']
 OBJECT_STORAGE_BUCKET = config['object_storage_subscription']['bucket']
@@ -50,6 +48,8 @@ LOG_LEVEL = config['logging']['level']
 NUM_USERS = config['threads']['num_users']
 MAX_THREADS = config['threads']['max_threads']
 ACTIONS_PER_USER = config['threads']['actions_per_user']
+PARETO_CONFIG = config['threads']['pareto_distribution']
+RETURNING_USER_PROBABILITY = config['threads']['returning_user_probability']
 
 # API 서버 정보
 API_BASE_URL = config['api']['base_url']
@@ -58,6 +58,19 @@ API_ENDPOINTS = {k.upper(): v for k, v in config['api']['endpoints'].items()}
 
 # 요청 간 대기 시간 범위 (초)
 TIME_SLEEP_RANGE = (config['api']['time_sleep_range']['min'], config['api']['time_sleep_range']['max'])
+
+# 플래시 세일 설정
+FLASH_SALE_CONFIG = config.get('flash_sale', {})
+
+# 지역 및 기기 정보
+REGIONS = config.get('regions', [])
+DEVICE_TYPES = config.get('device_types', [])
+
+# 페이지 체류 시간 범위 (초)
+DWELL_TIME_RANGE = (
+    config['dwell_time_range']['min'],
+    config['dwell_time_range']['max']
+)
 
 # 나이 구간 임계값
 AGE_THRESHOLD_YOUNG = config['age_threshold']['young']
@@ -68,31 +81,18 @@ AGE_THRESHOLD_MIDDLE = config['age_threshold']['middle']
 #################################
 STATE_TRANSITIONS = {
     "Anon_NotRegistered": {
-        "Anon_NotRegistered": 0.3,
-        "Anon_Registered": 0.5,
-        "Unregistered": 0.0,
-        "Done": 0.2
+        "Anon_NotRegistered": 0.3, "Anon_Registered": 0.5, "Done": 0.2
     },
     "Anon_Registered": {
-        "Anon_Registered": 0.3,
-        "Logged_In": 0.5,
-        "Unregistered": 0.1,
-        "Done": 0.1
+        "Anon_Registered": 0.3, "Logged_In": 0.5, "Unregistered": 0.1, "Done": 0.1
     },
     "Logged_In": {
-        "Logged_In": 0.7,
-        "Logged_Out": 0.1,
-        "Unregistered": 0.1,
-        "Done": 0.1
+        "Logged_In": 0.7, "Logged_Out": 0.1, "Unregistered": 0.1, "Done": 0.1
     },
     "Logged_Out": {
-        "Unregistered": 0.1,
-        "Anon_Registered": 0.1,
-        "Done": 0.8
+        "Unregistered": 0.1, "Anon_Registered": 0.1, "Done": 0.8
     },
-    "Unregistered": {
-        "Done": 1.0
-    },
+    "Unregistered": {"Done": 1.0},
     "Done": {}
 }
 
@@ -100,58 +100,24 @@ STATE_TRANSITIONS = {
 # 비로그인 하위머신 전이 표
 #################################
 ANON_SUB_TRANSITIONS = {
-    "Anon_Sub_Initial": {
-        "Anon_Sub_Main": 1.0,
-        "Anon_Sub_Products": 0.2,
-        "Anon_Sub_Categories": 0.2,
-        "Anon_Sub_Search": 0.2,
-        "Anon_Sub_Done": 0.2
-    },
+    "Anon_Sub_Initial": {"Anon_Sub_Main": 1.0},
     "Anon_Sub_Main": {
-        "Anon_Sub_Main": 0.1,
-        "Anon_Sub_Products": 0.2,
-        "Anon_Sub_Search": 0.2,
-        "Anon_Sub_Categories": 0.2,
-        "Anon_Sub_Error": 0.1,
-        "Anon_Sub_Done": 0.2
+        "Anon_Sub_Products": 0.4, "Anon_Sub_Search": 0.3, "Anon_Sub_Categories": 0.2, "Anon_Sub_Done": 0.1
     },
     "Anon_Sub_Products": {
-        "Anon_Sub_ViewProduct": 0.2,
-        "Anon_Sub_Products": 0.2,
-        "Anon_Sub_Categories": 0.1,
-        "Anon_Sub_Search": 0.2,
-        "Anon_Sub_Error": 0.1,
-        "Anon_Sub_Done": 0.2
+        "Anon_Sub_ViewProduct": 0.5, "Anon_Sub_Main": 0.3, "Anon_Sub_Done": 0.2
     },
     "Anon_Sub_ViewProduct": {
-        "Anon_Sub_Products": 0.2,
-        "Anon_Sub_Search": 0.2,
-        "Anon_Sub_Categories": 0.1,
-        "Anon_Sub_Error": 0.1,
-        "Anon_Sub_Done": 0.4
+        "Anon_Sub_Products": 0.5, "Anon_Sub_Done": 0.5
     },
     "Anon_Sub_Categories": {
-        "Anon_Sub_CategoryList": 0.4,
-        "Anon_Sub_Search": 0.2,
-        "Anon_Sub_Error": 0.1,
-        "Anon_Sub_Done": 0.3
+        "Anon_Sub_CategoryList": 0.6, "Anon_Sub_Main": 0.4
     },
     "Anon_Sub_CategoryList": {
-        "Anon_Sub_Categories": 0.2,
-        "Anon_Sub_ViewProduct": 0.3,
-        "Anon_Sub_Error": 0.1,
-        "Anon_Sub_Done": 0.4
+        "Anon_Sub_Products": 0.7, "Anon_Sub_Done": 0.3
     },
     "Anon_Sub_Search": {
-        "Anon_Sub_ViewProduct": 0.3,
-        "Anon_Sub_Search": 0.2,
-        "Anon_Sub_Error": 0.1,
-        "Anon_Sub_Done": 0.4
-    },
-    "Anon_Sub_Error": {
-        "Anon_Sub_Main": 0.3,
-        "Anon_Sub_Products": 0.2,
-        "Anon_Sub_Done": 0.5
+        "Anon_Sub_ViewProduct": 0.6, "Anon_Sub_Done": 0.4
     },
     "Anon_Sub_Done": {}
 }
@@ -160,140 +126,37 @@ ANON_SUB_TRANSITIONS = {
 # 로그인 하위머신 전이 표
 #################################
 LOGGED_SUB_TRANSITIONS = {
-    # 진입점
-    "Login_Sub_Initial": {
-        "Login_Sub_Main": 0.9,
-        "Login_Sub_Error": 0.1
-    },
-    
-    # 메인 페이지
+    "Login_Sub_Initial": {"Login_Sub_Main": 1.0},
     "Login_Sub_Main": {
-        "Login_Sub_Main": 0.1,
-        "Login_Sub_Products": 0.2,
-        "Login_Sub_ViewProduct": 0.2,
-        "Login_Sub_Search": 0.1,
-        "Login_Sub_Categories": 0.1,
-        "Login_Sub_CheckoutHistory": 0.1,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_Products": 0.3, "Login_Sub_ViewProduct": 0.2, "Login_Sub_Search": 0.1, "Login_Sub_ViewCart": 0.2, "Login_Sub_CheckoutHistory": 0.1, "Login_Sub_Done": 0.1
     },
-    
-    # 상품 목록
     "Login_Sub_Products": {
-        "Login_Sub_Main": 0.1,
-        "Login_Sub_Products": 0.2,
-        "Login_Sub_ViewProduct": 0.2,
-        "Login_Sub_Categories": 0.1,
-        "Login_Sub_Search": 0.2,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_ViewProduct": 0.6, "Login_Sub_Main": 0.2, "Login_Sub_Done": 0.2
     },
-    
-    # 상품 상세
     "Login_Sub_ViewProduct": {
-        "Login_Sub_Main": 0.1,
-        "Login_Sub_ViewProduct": 0.1,
-        "Login_Sub_Products": 0.1,
-        "Login_Sub_Search": 0.1,
-        "Login_Sub_Categories": 0.1,
-        "Login_Sub_ViewCart": 0.1,
-        "Login_Sub_CartAdd": 0.1,
-        "Login_Sub_Checkout": 0.1,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_CartAdd": 0.7, "Login_Sub_Products": 0.2, "Login_Sub_Done": 0.1
     },
-    
-    # 검색
     "Login_Sub_Search": {
-        "Login_Sub_Main": 0.2,
-        # 상품명을 정확하게가 아니라 상품 목록을 검색할 수도 있다. ex)모니터
-        "Login_Sub_Products": 0.2,
-        # 카테고리를 검색할 수도 있다. ex) 가전제품, 식품 
-        "Login_Sub_Categories": 0.1, 
-        "Login_Sub_ViewProduct": 0.3,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_ViewProduct": 0.8, "Login_Sub_Done": 0.2
     },
-    
-    # 카테고리 목록
-    "Login_Sub_Categories": {
-        "Login_Sub_Main": 0.2,
-        "Login_Sub_CategoryList": 0.3,
-        "Login_Sub_Search": 0.3,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
-    },
-    
-    # 특정 카테고리 상품 목록
-    "Login_Sub_CategoryList": {
-        "Login_Sub_Main": 0.2,
-        "Login_Sub_Categories": 0.3,
-        "Login_Sub_ViewProduct": 0.3,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
-    },
-    
-    # 장바구니 보기
     "Login_Sub_ViewCart": {
-        "Login_Sub_Main": 0.3,
-        "Login_Sub_CartRemove": 0.2,
-        "Login_Sub_Checkout": 0.3,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_Checkout": 0.6, "Login_Sub_CartRemove": 0.2, "Login_Sub_Main": 0.2
     },
-    
-    # 결제 이력 보기
     "Login_Sub_CheckoutHistory": {
-        "Login_Sub_Main": 0.4,
-        "Login_Sub_ViewCart": 0.2,
-        "Login_Sub_CartAdd": 0.2,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_Main": 0.8, "Login_Sub_Done": 0.2
     },
-    
-    # 장바구니에 상품 추가
     "Login_Sub_CartAdd": {
-        "Login_Sub_Main": 0.2,
-        "Login_Sub_ViewCart": 0.3,
-        "Login_Sub_Checkout": 0.3,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_ViewCart": 0.5, "Login_Sub_Products": 0.3, "Login_Sub_Done": 0.2
     },
-    
-    # 장바구니 상품 제거
     "Login_Sub_CartRemove": {
-        "Login_Sub_Main": 0.3,
-        "Login_Sub_ViewCart": 0.4,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.2
+        "Login_Sub_ViewCart": 0.8, "Login_Sub_Done": 0.2
     },
-    
-    # 결제
     "Login_Sub_Checkout": {
-        "Login_Sub_Main": 0.2,
-        "Login_Sub_CheckoutHistory": 0.3,
-        "Login_Sub_ViewCart": 0.1,
-        "Login_Sub_AddReview": 0.2,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_AddReview": 0.5, "Login_Sub_Main": 0.3, "Login_Sub_Done": 0.2
     },
-    
-    # 리뷰 작성
     "Login_Sub_AddReview": {
-        "Login_Sub_Main": 0.3,
-        "Login_Sub_AddReview": 0.2,
-        "Login_Sub_ViewCart": 0.1,
-        "Login_Sub_CheckoutHistory": 0.2,
-        "Login_Sub_Error": 0.1,
-        "Login_Sub_Done": 0.1
+        "Login_Sub_Main": 0.7, "Login_Sub_Done": 0.3
     },
-    
-    # 에러
-    "Login_Sub_Error": {
-        "Login_Sub_Done": 1.0
-    },
-    
-    # 종료
     "Login_Sub_Done": {}
 }
 
@@ -321,3 +184,10 @@ SEARCH_KEYWORDS = [
     "Coffee", "Mouse", "Sneakers", "Bag", "Sunglasses", "Mug",
     "iphon", "labtop", "rayban"
 ]
+
+#################################
+# 시간대 & 부하 계수 설정
+#################################
+_TIME_SEGMENTS_RAW = config.get('time_segments', {})
+TIME_SEGMENTS = {name: (seg['start'], seg['end']) for name, seg in _TIME_SEGMENTS_RAW.items()}
+SEGMENT_FACTORS = {name: tuple(seg['factor_range']) for name, seg in _TIME_SEGMENTS_RAW.items()}
