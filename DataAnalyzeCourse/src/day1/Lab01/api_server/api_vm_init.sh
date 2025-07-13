86bca9fdc051fdcf895eca38fec38730da8b41bd
cjm0423 <87014584+cjm0423@users.noreply.github.com>
2025-07-13 15:22:06 +0900

    Update api_vm_init.sh

--- DataAnalyzeCourse/src/day1/Lab01/api_server/api_vm_init.sh
+++ DataAnalyzeCourse/src/day1/Lab01/api_server/api_vm_init.sh
@@ -16,7 +16,7 @@ export KAFKA_TOPIC_NAME="nginx-topic"
 
 # 로그 및 환경 설정
 export LOGSTASH_ENV_FILE="/etc/default/logstash"
-export ENV_SETUP_SCRIPT_URL="https://github.com/kakaocloud-edu/tutorial/raw/refs/heads/main/DataAnalyzeCourse/src/day1/Lab01/api_server/api_env_setup.sh"
+export ENV_SETUP_SCRIPT_URL="https://github.com/kakaocloud-edu/tutorial/raw/refs/heads/tg_lsh_per/DataAnalyzeCourse/src/day1/Lab01/api_server/api_env_setup.sh"
 
 echo "kakaocloud: 1. api server VM 환경 설정용 api_env_setup.sh 다운로드"
 curl --output /dev/null --silent --head --fail "$ENV_SETUP_SCRIPT_URL" || {
