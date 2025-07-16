bde61f516e7a582ff21199e2f3ad94e942b2f57f
cjm0423 <87014584+cjm0423@users.noreply.github.com>
2025-07-16 17:39:20 +0900

    Update api_avro_setup.sh

--- DataAnalyzeCourse/src/day1/Lab01/api_server/api_avro_setup.sh
+++ DataAnalyzeCourse/src/day1/Lab01/api_server/api_avro_setup.sh
@@ -8,7 +8,7 @@ SCHEMA_REGISTRY_PROP="${CONFLUENT_HOME}/etc/schema-registry/schema-registry.prop
 SYSTEMD_SR_UNIT="/etc/systemd/system/schema-registry.service"
 LOGSTASH_SCHEMA_DIR="/etc/logstash/schema"
 LOGSTASH_CONF="/etc/logstash/conf.d/logs-to-kafka.conf"
-AVSC_FILE="${LOGSTASH_SCHEMA_DIR}/nginx.avsc"
+AVSC_FILE="${LOGSTASH_SCHEMA_DIR}/nginx_log.avsc"
 
 # 1. Java & Confluent 설치
 echo "apt 업데이트 및 Java 설치"
@@ -56,8 +56,8 @@ sudo mkdir -p /etc/logstash/schema || {
     echo "kakaocloud: Avro 스키마 및 Logstash 설정 파일 생성 실패"; exit 1;
 }
 
-sudo cp /home/ubuntu/tutorial/DataAnalyzeCourse/src/day1/Lab01/api_server/nginx.avsc /etc/logstash/schema/nginx.avsc || {
-    echo "kakaocloud: nginx.avsc 복사 실패"; exit 1;
+sudo cp /home/ubuntu/tutorial/DataAnalyzeCourse/src/day1/Lab01/api_server/nginx_log.avsc /etc/logstash/schema/nginx_log.avsc || {
+    echo "kakaocloud: nginx_log.avsc 복사 실패"; exit 1;
 }
 sudo cp /home/ubuntu/tutorial/DataAnalyzeCourse/src/day1/Lab01/api_server/logs-to-kafka.conf /etc/logstash/conf.d/logs-to-kafka.conf || {
     echo "kakaocloud: logs-to-kafka.conf 복사 실패"; exit 1;
