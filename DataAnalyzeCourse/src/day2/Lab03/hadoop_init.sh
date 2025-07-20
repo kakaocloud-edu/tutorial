b4eb7a0fff3969bb25d709f3b2de7c23583b4687
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-07-20 17:43:56 +0900

    Update hadoop_init.sh

--- DataAnalyzeCourse/src/day2/Lab03/hadoop_init.sh
+++ DataAnalyzeCourse/src/day2/Lab03/hadoop_init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # hadoop_init.sh
 sleep 600
-set -e  # 오류 발생 시 스크립트 종료
+set -e
 
-echo "kakaocloud: 스크립트 다운로드 사이트 유효성 검사 시작"
+# 스크립트 다운로드 사이트 유효성 검사
 curl --output /dev/null --silent --head --fail "https://github.com/kakaocloud-edu/tutorial/raw/refs/heads/main/DataAnalyzeCourse/src/day2/Lab03/hadoop-init.sh" || { echo "kakaocloud: Script download site is not valid"; exit 1; }
 
+# 스크립트 다운로드 후 실행
 wget https://github.com/kakaocloud-edu/tutorial/raw/refs/heads/main/DataAnalyzeCourse/src/day2/Lab03/hadoop-init.sh
 chmod +x tg_full_setup.sh
 sudo -E ./tg_full_setup.sh
