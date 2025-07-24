05e5e6ca7c4a45a3a19ac454eb0da22ad2b18bf0
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-07-24 10:18:24 +0900

    Add files via upload

---
+++ DataAnalyzeCourse/src/day2/Lab03/hadoop_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+sleep 600
+
+# extensions.loadList 업데이트
+echo "kakaocloud: extensions.loadList 업데이트"
+sudo sed -i 's|^druid.extensions.loadList=.*|druid.extensions.loadList=["druid-avro-extensions","druid-parquet-extensions","mysql-metadata-storage","druid-hdfs-storage","druid-kafka-indexing-service","druid-datasketches","druid-multi-stage-query"]|' \
+  /opt/apache-druid-33.0.0/conf/druid/cluster/_common/common.runtime.properties || { echo "kakaocloud: cluster/_common 업데이트 실패"; exit 1; }
+sudo sed -i 's|^druid.extensions.loadList=.*|druid.extensions.loadList=["druid-avro-extensions","druid-hdfs-storage","druid-kafka-indexing-service","druid-datasketches","druid-multi-stage-query"]|' \
+  /opt/apache-druid-33.0.0/conf/druid/auto/_common/common.runtime.properties || { echo "kakaocloud: auto/_common 업데이트 실패"; exit 1; }
+
+echo "kakaocloud: Setup 완료"
