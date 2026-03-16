cf136f1fe8b866e8d4dfa440a465a984136510bf
llokr1 <ghdwlsrl100@gachon.ac.kr>
2026-03-16 14:05:32 +0900

    Update script.sql
    - 계정 권한 부여 프로시저 파라미터에 맞게 수정

--- AdvancedCourse/src/helm/files/sql/script.sql
+++ AdvancedCourse/src/helm/files/sql/script.sql
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS access (
 
 	INSERT INTO access (date) VALUES ('hello:)');
 
-CALL mysql.mnms_grant_right_user('admin', '%', 'all', '*', '*');
+CALL mysql.mnms_grant_right_user('admin', 'all', '*', '*');
 
 ALTER USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'admin1234';
